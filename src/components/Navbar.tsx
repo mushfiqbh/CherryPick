68c55f219fd66812c940fa749508269e50bd1d2f
mushfiqbh <mushfiqbh@gmail.com>
2025-03-31 13:15:19 +0600

    Added New Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,12 +9,14 @@ const Navbar = () => {
   const { isSeller, router } = useAppContext();
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+    <nav className="min-h-16 flex items-center justify-between px-6 md:px-16 lg:px-32 py-0 border-b border-gray-300 text-gray-700">
       <Image
-        className="cursor-pointer w-28 md:w-32"
+        className="cursor-pointer w-36 md:w-32"
         onClick={() => router.push("/")}
         src={assets.logo}
         alt="logo"
+        height={36}
+        width={144}
       />
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">
